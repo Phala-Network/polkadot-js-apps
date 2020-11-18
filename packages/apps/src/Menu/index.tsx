a87ad3960309844751ec88f46e0fd17773700e50
h4x3rotab <h4x3rotab@gmail.com>
2020-11-18 22:19:25 +0800

    Update theme color and phala url

--- packages/apps/src/Menu/index.tsx
+++ packages/apps/src/Menu/index.tsx
@@ -30,8 +30,8 @@ const disabledLog = new Map<string, string>();
 
 function createExternals (t: TFunction): ItemRoute[] {
   return [
-    { href: 'https://github.com/polkadot-js/apps', icon: 'code-branch', name: 'github', text: t<string>('nav.github', 'GitHub', { ns: 'apps-routing' }) },
-    { href: 'https://wiki.polkadot.network', icon: 'book', name: 'wiki', text: t<string>('nav.wiki', 'Wiki', { ns: 'apps-routing' }) }
+    { href: 'https://github.com/Phala-Network/', icon: 'code-branch', name: 'github', text: t<string>('nav.github', 'GitHub', { ns: 'apps-routing' }) },
+    { href: 'https://wiki.phala.network/', icon: 'book', name: 'wiki', text: t<string>('nav.wiki', 'Wiki', { ns: 'apps-routing' }) }
   ];
 }
 
