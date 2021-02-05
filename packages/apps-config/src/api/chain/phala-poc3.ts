de00795b25cb853a9683189c0473b9f09a2cf640
Jiacheng <gh@kt.je>
2021-02-06 01:13:19 +0800

    Update phala-poc3.ts

--- packages/apps-config/src/api/chain/phala-poc3.ts
+++ packages/apps-config/src/api/chain/phala-poc3.ts
@@ -6,17 +6,17 @@ export default {
   "LookupSource": "GenericLookupSource",
   "EthereumAddress": "H160",
   "EcdsaSignature": "[u8; 65]",
-	"EthereumTxHash": "H256",
-	"WorkerStateEnum": {
-		"_enum": {
-			"Empty": null,
-			"Free": null,
-			"Gatekeeper": null,
-			"MiningPending": null,
-			"Mining": "BlockNumber",
-			"MiningStopping": null
-		}
-	},
+  "EthereumTxHash": "H256",
+  "WorkerStateEnum": {
+    "_enum": {
+      "Empty": null,
+      "Free": null,
+      "Gatekeeper": null,
+      "MiningPending": null,
+      "Mining": "BlockNumber",
+      "MiningStopping": null
+    }
+  },
   "WorkerInfo": {
     "machineId": "Vec<u8>",
     "pubkey": "Vec<u8>",
@@ -33,23 +33,34 @@ export default {
     "payoutPrefs": "PayoutPrefs"
   },
   "PayoutPrefs": {
-      "commission": "u32",
-      "target": "AccountId"
-	},
-	"BlockRewardInfo": {
-		"seed": "U256",
-		"onlineTarget": "U256",
-		"computeTarget": "U256"
-	},
-	"RoundInfo": {
-		"round": "u32",
-		"startBlock": "BlockNumber"
-	},
-	"RoundStats": {
-		"round": "u32",
-		"onlineWorkers": "u32",
-		"computeWorkers": "u32",
-		"fracTargetOnlineReward": "u32",
-		"totalPower": "u32"
+    "commission": "u32",
+    "target": "AccountId"
+  },
+  "BlockRewardInfo": {
+    "seed": "U256",
+    "onlineTarget": "U256",
+    "computeTarget": "U256"
+  },
+  "RoundInfo": {
+    "round": "u32",
+    "startBlock": "BlockNumber"
+  },
+  "RoundStats": {
+    "round": "u32",
+    "onlineWorkers": "u32",
+    "computeWorkers": "u32",
+    "fracTargetOnlineReward": "u32",
+    "totalPower": "u32",
+    "fracTargetComputeReward": "u32"
+  },
+  "MinerStatsDelta": {
+    "numWorker": "i32",
+    "numPower": "i32"
+  },
+  "PayoutReason": {
+    "_enum": {
+	  "OnlineReward": null,
+	  "ComputeReward": null
 	}
+  }
 };
